c4e5912e4f7bb2cef6dfc0d8d585fa162d452944
Poeli Bojorquez <pbojorquez@poeli.com>
2019-02-11 12:49:08 +0100

    Sign-up/Log-in environment set up

--- models/story-model.js
+++ models/story-model.js
@@ -7,7 +7,7 @@ const storySchema = new Schema(
     name: { type: String, required: true },
     profession: { type: String, required: true },
     description: { type: String, required: true, minlength: 100 },
-    pictureUrl: { type: String, required: true },
+    pictureUrl: { type: String },
     year: { type: String },
     country: { type: String, required: true },
     quote: { type: String, required: true }
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -11,13 +11,13 @@ router.get("/signup", (req, res, next) => {
 });
 
 router.post("/process-signup", (req, res, next) => {
-  const { fullName, email, originalPassword } = req.body;
+  const { userName, email, originalPassword } = req.body;
 
   // enforce password rules (can't be empty and must have a digit)
-  if (!originalPassword || !originalPassword.match(/[0-9]/)) {
+  if (!originalPassword) {
     // req.flash sends feedback message before redirect
     // (it's defined by the "connect-flash" npm package)
-    req.flash("error", "Password can't be blank and must contain a number.");
+    req.flash("error", "Password can't be blank.");
     //redirect to the SIGNUP PAGE if the password is BAD
     res.redirect("/signup");
     // use return to STOP the function here if the password is BAD
@@ -27,7 +27,7 @@ router.post("/process-signup", (req, res, next) => {
   // encrypt the user's password before saving it
   const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
 
-  User.create({ fullName, email, encryptedPassword })
+  User.create({ userName, email, encryptedPassword })
     .then(() => {
       // req.flash sends feedback message before redirect
       // (it's defined by the "connect-flash" npm package)
@@ -36,6 +36,63 @@ router.post("/process-signup", (req, res, next) => {
       res.redirect("/");
     })
     .catch(err => next(err));
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth-views/login-form.hbs");
+});
+
+router.post("/process-login", (req, res, next) => {
+  const { userName, originalPassword } = req.body;
+
+  User.findOne({ userName: { $eq: userName } })
+    .then(userDoc => {
+      if (!userDoc) {
+        // req.flash sends feedback message before redirect
+        // (it's defined by the "connect-flash" npm package)
+        req.flash("error", "Username is incorrect.");
+        // redirect to LOGIN PAGE if result is NULL (no account with that email)
+        res.redirect("/login");
+        // user return to STOP the function here if the PASSWORD is BAD
+        return;
+      }
+
+      const { encryptedPassword } = userDoc;
+      console.log(originalPassword, encryptedPassword);
+      // validate the password by using bcrypt.compareSync()
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        // req.flash sends feedback message before redirect
+        // (it's defined by the "connect-flash" npm package)
+        req.flash("error", "Password is incorrect.");
+        // redirect to LOGIN PAGE if the password don't match
+        res.redirect("/login");
+        // user return to STOP the function here if the PASSWORD is BAD
+        return;
+      }
+
+      // email & password are CORRECT!
+      // if we MANUALLY managed the user session
+      // req.session.userId = userDoc._id;
+
+      // instead we will use PASSPORT - an npm package for managing user sessions
+      // req.logIn is a Passport method that calls serializeUser()
+      // (that saves the USER ID in teh session which means we are logged-in)
+      req.logIn(userDoc, () => {
+        // req.flash sends feedback message before redirect
+        // (it's defined by the "connect-flash" npm package)
+        req.flash("success", "Log in successful! 👍");
+        res.redirect("/");
+      });
+    })
+    .catch(err => next(err));
+});
+
+router.get("/logout", (req, res, next) => {
+  // req.logOut();
+  req.logOut();
+
+  req.flash("success", "Logged out successfully! 🔑");
+  res.redirect("/");
 });
 
 module.exports = router;
