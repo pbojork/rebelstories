3e67acf31b575d5b9442968ce61a6d39bca209e4
wowfranzik <7francescagatti@gmail.com>
2019-02-13 14:39:55 +0100

    add entry-router functions

--- routes/entry-router.js
+++ routes/entry-router.js
@@ -22,20 +22,51 @@ router.get("/mood1", (req, res, next) => {
 router.get("/:keyword/mood2", (req, res, next) => {
   Story.find({ mood1: { $eq: req.params.keyword } })
     .then(mood1Results => {
-      // in mood-page2.hbs display only the mood2 keywords relatives to mood1 results
-
       //info to send to mood2.hbs
+      const arr = [];
+      mood1Results.forEach(element => {
+        console.log(element.mood2);
+        // const filtered = arr.filter(arr => arr.length);
+        // return filtered;
+        // // arr.filter((elem, index, self) => {
+        // //   return index == self.indexOf(elem);
+
+        // filtered.push(element.mood2[0], element.mood2[1]);
+        arr.push(element.mood2[0], element.mood2[1]);
+      });
+
+      //   const filteredKeywords = arr.filter(elem, index, self) => {
+      //     return index == self.indexOf(elem);
+
+      //     return filteredKeywords;
+      // });
+
+      console.log(arr);
+      res.locals.myArray = arr;
       res.render("entry-views/mood-page2.hbs");
     })
-    .catch();
+    .catch(err => next(err));
 });
 
-router.get("/:mood1/:mood2/story", () => {
+router.get("/:keyword2/story", () => {
+  Story.find({ mood2: { $eq: req.params.keyword2 } })
+    .then(mood2Results => {
+      console.log(mood2Results);
+      // const arr2 = [];
+
+      // mood2Results.forEach(element => {
+      //   console.log(element);
+      //   arr2.push(element);
+      // });
+      // console.log(arr2);
+      // res.locals.myArray2 = arr2;
+      // res.render("entry-views/story.hbs");
+    })
+    .catch(err => next(err));
   // after clicking on one keyword in mood2.hbs, random pick a story and display it in story.hbs
 
   // const randomStory = Math.floor(math.random() * mood1Results.length);
   // console.log(mood1Results[randomStory]);
-  res.send("story");
 });
 
 module.exports = router;
