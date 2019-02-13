08a42977e4bda176ba76d395a240c5c172885432
Poeli Bojorquez <pbojorquez@poeli.com>
2019-02-13 12:07:54 +0100

    css test

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -10,13 +10,15 @@ router.get("/signup", (req, res, next) => {
   res.render("auth-views/signup-form.hbs");
 });
 
-router.get("/mood1", (req, res, next) => {
-  res.render("entry-views/mood-page1.hbs");
-});
+// Gets mood pages when creating a new username.
+// Do we erase from here???
+// router.get("/mood1", (req, res, next) => {
+//   res.render("entry-views/mood-page1.hbs");
+// });
 
-router.get("/mood2", (req, res, next) => {
-  res.render("entry-views/mood-page2.hbs");
-});
+// router.get("/mood2", (req, res, next) => {
+//   res.render("entry-views/mood-page2.hbs");
+// });
 
 router.post("/process-signup", (req, res, next) => {
   const { userName, email, originalPassword } = req.body;
--- routes/entry-router.js
+++ routes/entry-router.js
@@ -1,9 +1,29 @@
 const express = require("express");
-
+const Story = require("../models/story-model.js");
 const router = express.Router();
 
 router.get("/home", (req, res, next) => {
   res.render("entry-views/home.hbs");
+});
+
+router.get("/mood1", (req, res, next) => {
+  res.render("entry-views/mood-page1.hbs");
+
+  // // find the stories owned by the logged-in user
+  // Story.find({ publisher: { $eq: req.user._id } })
+  //   // sort by newest first
+  //   .sort({ createdAt: -1 })
+  //   // first 10 results
+  //   .limit(10)
+  //   .then(storyResults => {
+  //     res.locals.storyArray = storyResults;
+  //     res.render("story-views/personal-list.hbs");
+  //   })
+  //   .catch(err => next(err));
+});
+
+router.get("/mood2", (req, res, next) => {
+  res.render("entry-views/mood-page2.hbs");
 });
 
 module.exports = router;
